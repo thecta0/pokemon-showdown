c24fafaec42faa1d1c086ff0f4f7622c63b8022f
thecta0 <159853084+thecta0@users.noreply.github.com>
2025-05-15 20:13:03 -0400

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -5613,12 +5613,12 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		onPrepareHit(source, target, move) {
 			if (this.effectState.protean === source.previouslySwitchedIn) return;
 			if (move.hasBounced || move.flags['futuremove'] || move.sourceEffect === 'snatch' || move.callsMove) return;
-			const type = ['Dragon', move.type];
+			const type = move.type;
                         const possibleTypes = ['Fire', 'Electric', 'Ice', 'Flying'];
+			const finalType = ['Dragon', type];
 			if (type && type !== '???' && source.getTypes().join() !== type && possibleTypes.includes(type)) {
 				if (!source.setType(type)) return; 
-				this.add('-start', source, 'typechange', type, '[from] ability: Limitless');
-                               // this.add('-start', target, 'typeadd', 'Dragon', '[from] ability: Limitless');
+				this.add('-start', source, 'typechange', finalType, '[from] ability: Limitless');
 			}
 		},
 		flags: {},
